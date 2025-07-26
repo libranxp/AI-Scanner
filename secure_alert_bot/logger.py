df85a771fc0662e039bb34df4d87d904a73357a9
libranxp <130633300+libranxp@users.noreply.github.com>
2025-07-26 23:08:15 +0100

    logger.py

--- secure_alert_bot/logger.py
+++ secure_alert_bot/logger.py
@@ -1,5 +1,18 @@
 import logging
 import sys
 
-def setup_logger():
-    logger =
+def setup_logger(name: str = "secure_alert_bot"):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "[%(asctime)s] [%(levelname)s] %(message)s", "%Y-%m-%d %H:%M:%S"
+    )
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+
+    if not logger.hasHandlers():
+        logger.addHandler(console_handler)
+
+    return logger
