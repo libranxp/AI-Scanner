2f5435794d695037ceba312c79e41823042e8d0c
libranxp <130633300+libranxp@users.noreply.github.com>
2025-07-27 01:18:53 +0100

    config.py

--- secure_alert_bot/config.py
+++ secure_alert_bot/config.py
@@ -1,16 +1,6 @@
 import os
-from datetime import time
-
-def parse_time(t_str):
-    """Parse HH:MM string to datetime.time"""
-    try:
-        h, m = map(int, t_str.split(":"))
-        return time(hour=h, minute=m)
-    except Exception:
-        return None
 
 class Config:
-    # Telegram & Channel IDs
     TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
     TELEGRAM_CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
     PENNY_STOCK_CHANNEL_ID = os.environ['PENNY_STOCK_CHANNEL_ID']
@@ -20,23 +10,7 @@ class Config:
     VALIDATION_CHANNEL_ID = os.environ['VALIDATION_CHANNEL_ID']
     WRAPUP_CHANNEL_ID = os.environ['WRAPUP_CHANNEL_ID']
 
-    # API Keys
-    LUNARCRUSH_API_KEY = os.environ['LUNARCRUSH_API_KEY']
-    FMP_API_KEY = os.environ['FMP_API_KEY']
-    FINNHUB_API_KEY = os.environ['FINNHUB_API_KEY']
-    COINMARKETCAL_API_KEY = os.environ['COINMARKETCAL_API_KEY']
-    ALPHAVANTAGE_API_KEY = os.environ['ALPHAVANTAGE_API_KEY']
+    # Removed LunarCrush API Key
+    # Added CoinGecko API URL (optional)
+    COINGECKO_API_URL = "https://api.coingecko.com/api/v3"
 
-    # Trading Time Windows (UTC)
-    PEAK_START_TIME = parse_time(os.getenv('PEAK_START_TIME', '12:30'))
-    PEAK_END_TIME = parse_time(os.getenv('PEAK_END_TIME', '15:30'))
-
-    # Scan intervals (seconds)
-    SCAN_INTERVAL_PENNY = int(os.getenv('SCAN_INTERVAL_PENNY', 600))
-    SCAN_INTERVAL_STOCK = int(os.getenv('SCAN_INTERVAL_STOCK', 600))
-    SCAN_INTERVAL_CRYPTO = int(os.getenv('SCAN_INTERVAL_CRYPTO', 600))
-
-    # Alert intervals (seconds)
-    ALERT_INTERVAL_PENNY_PEAK = int(os.getenv('ALERT_INTERVAL_PENNY_PEAK', 300))
-    ALERT_INTERVAL_PENNY_OFF = int(os.getenv('ALERT_INTERVAL_PENNY_OFF', 600))
-    ALERT_INTERVAL_CRYPTO = int(os.getenv('ALERT_INTERVAL_CRYPTO', 600))
