d0e7888a21cce55a2cc887d103a9c00de69c7cdc
libranxp <130633300+libranxp@users.noreply.github.com>
2025-07-27 12:51:44 +0100

    config.py

--- secure_alert_bot/config.py
+++ secure_alert_bot/config.py
@@ -1,16 +1,31 @@
+# config.py
 import os
 
 class Config:
-    TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
-    TELEGRAM_CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
-    PENNY_STOCK_CHANNEL_ID = os.environ['PENNY_STOCK_CHANNEL_ID']
-    STOCK_CHANNEL_ID = os.environ['STOCK_CHANNEL_ID']
-    CRYPTO_CHANNEL_ID = os.environ['CRYPTO_CHANNEL_ID']
-    WATCHLIST_CHANNEL_ID = os.environ['WATCHLIST_CHANNEL_ID']
-    VALIDATION_CHANNEL_ID = os.environ['VALIDATION_CHANNEL_ID']
-    WRAPUP_CHANNEL_ID = os.environ['WRAPUP_CHANNEL_ID']
+    TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+    TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+    STOCK_CHANNEL_ID = os.getenv("STOCK_CHANNEL_ID")
+    PENNY_STOCK_CHANNEL_ID = os.getenv("PENNY_STOCK_CHANNEL_ID")
+    CRYPTO_CHANNEL_ID = os.getenv("CRYPTO_CHANNEL_ID")
+    WATCHLIST_CHANNEL_ID = os.getenv("WATCHLIST_CHANNEL_ID")
+    VALIDATION_CHANNEL_ID = os.getenv("VALIDATION_CHANNEL_ID")
+    WRAPUP_CHANNEL_ID = os.getenv("WRAPUP_CHANNEL_ID")
+    FMP_API_KEY = os.getenv("FMP_API_KEY")
+    FINNHUB_API_KEY = os.getenv("FINNHUB_API_KEY")
+    COINMARKETCAL_API_KEY = os.getenv("COINMARKETCAL_API_KEY")
+    ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+    SCAN_INTERVAL_STOCK = int(os.getenv("SCAN_INTERVAL_STOCK", 600))
+    SCAN_INTERVAL_PENNY = int(os.getenv("SCAN_INTERVAL_PENNY", 600))
+    SCAN_INTERVAL_CRYPTO = int(os.getenv("SCAN_INTERVAL_CRYPTO", 600))
+    ALERT_INTERVAL_PENNY_PEAK = int(os.getenv("ALERT_INTERVAL_PENNY_PEAK", 300))
+    ALERT_INTERVAL_PENNY_OFF = int(os.getenv("ALERT_INTERVAL_PENNY_OFF", 600))
+    ALERT_INTERVAL_CRYPTO = int(os.getenv("ALERT_INTERVAL_CRYPTO", 600))
+    PEAK_START_TIME = os.getenv("PEAK_START_TIME", "12:30")
+    PEAK_END_TIME = os.getenv("PEAK_END_TIME", "15:30")
 
-    # Removed LunarCrush API Key
+ # Removed LunarCrush API Key
     # Added CoinGecko API URL (optional)
     COINGECKO_API_URL = "https://api.coingecko.com/api/v3"
 
+
+    
