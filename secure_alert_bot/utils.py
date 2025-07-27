e42cbd1b20b38a392008891a94beb2f244548e2e
libranxp <130633300+libranxp@users.noreply.github.com>
2025-07-27 23:24:31 +0100

    utils.py

--- secure_alert_bot/utils.py
+++ secure_alert_bot/utils.py
@@ -1,6 +1,7 @@
 from datetime import datetime, time as dtime
 import os
 
+
 def is_peak_hours():
     peak_start = os.getenv('PEAK_START_TIME', '09:15')
     peak_end = os.getenv('PEAK_END_TIME', '11:00')
@@ -12,36 +13,46 @@ def is_peak_hours():
 
     return peak_start_time <= current_time_utc <= peak_end_time
 
+
 def format_number(num):
-    if num >= 1_000_000:
-        return f"{num / 1_000_000:.2f}M"
-    elif num >= 1_000:
-        return f"{num / 1_000:.2f}K"
-    else:
+    try:
+        num = float(num)
+        if num >= 1_000_000:
+            return f"{num / 1_000_000:.2f}M"
+        elif num >= 1_000:
+            return f"{num / 1_000:.2f}K"
+        else:
+            return str(num)
+    except (ValueError, TypeError):
         return str(num)
 
+
 def build_alert_message(alert_type, data):
-    if alert_type == "penny_stock":
-        return (f"<b>Penny Stock Alert</b>\n"
-                f"Ticker: {data['ticker']}\n"
-                f"Price: ${data['price']}\n"
-                f"Change: {data['change']}%\n"
-                f"Volume: {format_number(data['volume'])}")
+    try:
+        if alert_type == "penny_stock":
+            return (f"<b>Penny Stock Alert</b>\n"
+                    f"Ticker: {data.get('ticker', 'N/A')}\n"
+                    f"Price: ${data.get('price', 'N/A')}\n"
+                    f"Change: {data.get('change', 'N/A')}%\n"
+                    f"Volume: {format_number(data.get('volume', 0))}")
 
-    elif alert_type == "stock":
-        return (f"<b>Stock Alert</b>\n"
-                f"Ticker: {data['ticker']}\n"
-                f"Price: ${data['price']}\n"
-                f"Change: {data['change']}%\n"
-                f"Volume: {format_number(data['volume'])}")
+        elif alert_type == "stock":
+            return (f"<b>Stock Alert</b>\n"
+                    f"Ticker: {data.get('ticker', 'N/A')}\n"
+                    f"Price: ${data.get('price', 'N/A')}\n"
+                    f"Change: {data.get('change', 'N/A')}%\n"
+                    f"Volume: {format_number(data.get('volume', 0))}")
 
-    elif alert_type == "crypto":
-        return (f"<b>Crypto Alert</b>\n"
-                f"Symbol: {data['symbol']}\n"
-                f"Price: ${data['price']}\n"
-                f"Change: {data['change']}%\n"
-                f"Volume: {format_number(data['volume'])}")
+        elif alert_type == "crypto":
+            return (f"<b>Crypto Alert</b>\n"
+                    f"Symbol: {data.get('symbol', 'N/A')}\n"
+                    f"Price: ${data.get('price', 'N/A')}\n"
+                    f"Change: {data.get('change', 'N/A')}%\n"
+                    f"Volume: {format_number(data.get('volume', 0))}")
 
-    else:
-        return "Invalid Alert Type"
+        else:
+            return "Invalid Alert Type"
+
+    except Exception as e:
+        return f"Error building alert message: {e}"
 
