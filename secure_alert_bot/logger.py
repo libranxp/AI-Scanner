44f1f6e3e9ac04358047e064b44ab79474a5ebfe
libranxp <130633300+libranxp@users.noreply.github.com>
2025-07-27 23:08:49 +0100

    logger.py

--- secure_alert_bot/logger.py
+++ secure_alert_bot/logger.py
@@ -1,13 +1,28 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
-def get_logger(name: str):
+
+def get_logger(name: str, log_file: str = None, level: int = logging.INFO):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(level)
 
     if not logger.handlers:
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+        # Console handler
+        console_handler = logging.StreamHandler()
+        console_formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+        console_handler.setFormatter(console_formatter)
+        logger.addHandler(console_handler)
+
+        # Optional File handler with rotation
+        if log_file:
+            file_handler = RotatingFileHandler(log_file, maxBytes=5 * 1024 * 1024, backupCount=3)
+            file_formatter = logging.Formatter(
+                '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+            )
+            file_handler.setFormatter(file_formatter)
+            logger.addHandler(file_handler)
 
     return logger
+
